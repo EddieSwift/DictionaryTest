79d09c83b0b46b8bcd1b8e9571c6ccece243b073
Eddie <eddiegalchenko@gmail.com>
2018-04-18 19:47:09 +0300

    Initial Commit

---
+++ DictionaryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DictionaryTest
+//
+//  Created by Eddie on 18.04.2018.
+//  Copyright © 2018 Eddie Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
